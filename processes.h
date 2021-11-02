efb89dac620df30c817d33a4abcc8e56ce869600
EdwardFedorenkov <79554504+EdwardFedorenkov@users.noreply.github.com>
2021-11-02 17:04:18 +0700

    Add files via upload

--- processes.h
+++ processes.h
@@ -1,7 +1,8 @@
 #pragma once
 
 #include <armadillo>
-#include <map>
+#include <tuple>
+#include <fstream>
 
 using namespace arma;
 using namespace std;
@@ -24,15 +25,32 @@ enum class DataType{
 	Cross_section
 };
 
+pair<string, string> ReadTextParams(const string& file_name){
+	ifstream file_params_names (file_name + ".txt");
+	string param_1 = "";
+	string param_2 = "";
+	if (file_params_names.is_open()){
+		getline(file_params_names, param_1);
+		getline(file_params_names, param_2);
+	    file_params_names.close();
+	}
+	return make_pair(param_1, param_2);
+}
+
 template<typename T>
 class ElementaryProcess{
 public:
-	ElementaryProcess(const DataType dd, const ProcessType pt, const string& file_name);
+	ElementaryProcess(const DataType dt, const ProcessType pt, const string& file_name) : data_type(dt), proc(pt),
+	param_names(ReadTextParams(file_name)){
+		vec params;
+		params.load(file_name + ".bin", raw_binary);
+
+	}
 
 private:
 	DataType data_type;
 	ProcessType proc;
+	pair<string, string> param_names;
 	T data;
-	map<size_t, string> param_to_name;
 };
 
