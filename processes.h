cab4468845ca8e881a6f25e1384f659498fc217e
EdwardFedorenkov <79554504+EdwardFedorenkov@users.noreply.github.com>
2021-11-12 19:02:13 +0700

    Add files via upload

--- processes.h
+++ processes.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <armadillo>
-#include <tuple>
 #include <iostream>
 #include <sstream>
 #include <iterator>
@@ -50,9 +49,56 @@ private:
 	const DataType dt;
 };
 
+class Charge_exchange : public ElementaryProcess{
+public:
+	Charge_exchange(const string& path) : ElementaryProcess(ProcessType::Charge_exchange, DataType::Rate_coefficient){
+
+	}
+
+	double ComputeDataTypeValue(const map<ParamsType, double>& params) const override{
+
+	}
+
+private:
+	vector<vector<double>> fitting_params;
+};
+
+class He_ionization : public ElementaryProcess{
+public:
+	He_ionization(const string& path) : ElementaryProcess(ProcessType::He_Ionization, DataType::Rate_coefficient){
+		fstream file(path, ios_base::in);
+		if(file.is_open()){
+			string line;
+			while(getline(file, line)){
+				istringstream buffer(line);
+				fitting_params.push_back(*istream_iterator<double>(buffer));
+			}
+		}
+	}
+
+	double ComputeDataTypeValue(const map<ParamsType, double>& params) const override{
+		double temperature = params.at(ParamsType::Temperature);
+		double T_min = fitting_params[0];
+		double T_max = fitting_params[1];
+		vector<double> logT_vec(fitting_params.size() - 2, 0.0);
+		if(T_max - temperature >= datum::eps and temperature - T_min >= datum::eps){
+			logT_vec[0] = log(temperature);
+			for(size_t i = 1; i < logT_vec.size(); ++i){
+				logT_vec[i] = logT_vec[i-1] * logT_vec[0];
+			}
+		}else
+			throw out_of_range("Electron temperature in He_ionization is out of range");
+		return exp(inner_product(fitting_params.begin() + 2, fitting_params.end(), logT_vec.begin(), 0.0));
+	}
+
+private:
+	vector<double> fitting_params;
+};
+
 class He_elastic : public ElementaryProcess{
 public:
-	He_elastic(const string& path) : ElementaryProcess(ProcessType::He_elastic, DataType::Diffusion_coefficient) {
+	He_elastic(const string& path, const double target_mass_) : ElementaryProcess(ProcessType::He_elastic, DataType::Diffusion_coefficient),
+	target_mass(target_mass_) {
 		fstream file(path, ios_base::in);
 		if(file.is_open()){
 			string line;
@@ -61,7 +107,7 @@ public:
 				vector<double> param((istream_iterator<double>(buffer)),
 						istream_iterator<double>());
 				energies.push_back(param[0]);
-				cross_sections.push_back(param[1]);
+				mt_cross_sections.push_back(param[1]);
 			}
 		}else
 			throw logic_error(path + " : file not found");
@@ -70,12 +116,32 @@ public:
 	double ComputeDataTypeValue(const map<ParamsType, double>& params) const override{
 		double temperature = params.at(ParamsType::Temperature);
 		double density = params.at(ParamsType::Density);
-
+		return 8.0 / (3 * sqrt(2 * datum::pi)) * datum::c_0 * 100 * density * temperature * temperature
+				* temperature / target_mass / sqrt(temperature / (datum::m_e * datum::c_0 * datum::c_0 / datum::eV) )
+				* ComputeIntOverEnergy(temperature);
 	}
 
 private:
+	double ComputeIntOverEnergy(const double T) const{
+		double result = 0;
+		for(size_t i = 0; i < energies.size() - 1; ++i){
+			double energy_step = energies[i+1] - energies[i];
+			double first_param = mt_cross_sections[i]*(1 + energies[i] / energy_step) - energies[i] / energy_step * mt_cross_sections[i+1];
+			first_param /= T;
+			double second_param = (mt_cross_sections[i+1] - mt_cross_sections[i]) / energy_step;
+			result += IntegralValue(energies[i+1] / T, first_param, second_param)
+					- IntegralValue(energies[i] / T, first_param, second_param);
+		}
+		return result;
+	}
+
+	double IntegralValue(const double x, const double param1, const double param2) const{
+		return exp(-x) * (-param1 * (x*x + 2*x + 2) - param2 * (x*x*x + 3*x*x + 6*x + 6));
+	}
+
+	double target_mass;
 	vector<double> energies;
-	vector<double> cross_sections;
+	vector<double> mt_cross_sections;
 };
 
 class HH_elastic : public ElementaryProcess{
